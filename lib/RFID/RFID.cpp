f0c52645712aa0eacec18a58b1d8df456928b12e
Matt Todd <matt.c.todd@gmail.com>
2016-03-05 15:41:51 -0600

    whitespace cleanup

--- lib/RFID/RFID.cpp
+++ lib/RFID/RFID.cpp
@@ -21,12 +21,12 @@
  */
 RFID::RFID(int chipSelectPin, int NRSTPD)
 {
-	_chipSelectPin = chipSelectPin;
-	
+  _chipSelectPin = chipSelectPin;
+
   pinMode(_chipSelectPin,OUTPUT);			// Set digital as OUTPUT to connect it to the RFID /ENABLE pin 
   digitalWrite(_chipSelectPin, LOW); 
-  
-  
+
+
   pinMode(NRSTPD,OUTPUT);					// Set digital pin, Not Reset and Power-down
   digitalWrite(NRSTPD, HIGH);
   _NRSTPD = NRSTPD;
@@ -35,35 +35,35 @@ RFID::RFID(int chipSelectPin, int NRSTPD)
  * User API
  ******************************************************************************/
 
- bool RFID::isCard() 
- {
-	unsigned char status;
-	unsigned char str[MAX_LEN];
-	
-	status = MFRC522Request(PICC_REQIDL, str);	
-    if (status == MI_OK) {
-		return true;
-	} else { 
-		return false; 
-	}
- }
- 
- bool RFID::readCardSerial(){
-	
-	unsigned char status;
-	unsigned char str[MAX_LEN];
-	
-	// Anti-colisión, devuelva el número de serie de tarjeta de 4 bytes
-	status = anticoll(str);
-	memcpy(serNum, str, 5);
-	
-	if (status == MI_OK) {
-		return true;
-	} else {
-		return false;
-	}
- 
- }
+bool RFID::isCard() 
+{
+  unsigned char status;
+  unsigned char str[MAX_LEN];
+
+  status = MFRC522Request(PICC_REQIDL, str);	
+  if (status == MI_OK) {
+    return true;
+  } else { 
+    return false; 
+  }
+}
+
+bool RFID::readCardSerial(){
+
+  unsigned char status;
+  unsigned char str[MAX_LEN];
+
+  // Anti-colisión, devuelva el número de serie de tarjeta de 4 bytes
+  status = anticoll(str);
+  memcpy(serNum, str, 5);
+
+  if (status == MI_OK) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
 
 /******************************************************************************
  * Dr.Leong   ( WWW.B2CQSHOP.COM )
@@ -71,52 +71,52 @@ RFID::RFID(int chipSelectPin, int NRSTPD)
 
 void RFID::init()
 {
-    digitalWrite(_NRSTPD,HIGH);
+  digitalWrite(_NRSTPD,HIGH);
 
-	reset();
-	 	
-	//Timer: TPrescaler*TreloadVal/6.78MHz = 24ms
-    writeMFRC522(TModeReg, 0x8D);		//Tauto=1; f(Timer) = 6.78MHz/TPreScaler
-    writeMFRC522(TPrescalerReg, 0x3E);	//TModeReg[3..0] + TPrescalerReg
-    writeMFRC522(TReloadRegL, 30);           
-    writeMFRC522(TReloadRegH, 0);
-	
-	writeMFRC522(TxAutoReg, 0x40);		//100%ASK
-	writeMFRC522(ModeReg, 0x3D);		// CRC valor inicial de 0x6363
+  reset();
 
-	//ClearBitMask(Status2Reg, 0x08);	//MFCrypto1On=0
-	//writeMFRC522(RxSelReg, 0x86);		//RxWait = RxSelReg[5..0]
-	//writeMFRC522(RFCfgReg, 0x7F);   	//RxGain = 48dB
+  //Timer: TPrescaler*TreloadVal/6.78MHz = 24ms
+  writeMFRC522(TModeReg, 0x8D);		//Tauto=1; f(Timer) = 6.78MHz/TPreScaler
+  writeMFRC522(TPrescalerReg, 0x3E);	//TModeReg[3..0] + TPrescalerReg
+  writeMFRC522(TReloadRegL, 30);           
+  writeMFRC522(TReloadRegH, 0);
 
-	antennaOn();		//Abre  la antena
-	
-	
+  writeMFRC522(TxAutoReg, 0x40);		//100%ASK
+  writeMFRC522(ModeReg, 0x3D);		// CRC valor inicial de 0x6363
+
+  //ClearBitMask(Status2Reg, 0x08);	//MFCrypto1On=0
+  //writeMFRC522(RxSelReg, 0x86);		//RxWait = RxSelReg[5..0]
+  //writeMFRC522(RFCfgReg, 0x7F);   	//RxGain = 48dB
+
+  antennaOn();		//Abre  la antena
+
+
 }
 void RFID::reset()
 {
-	writeMFRC522(CommandReg, PCD_RESETPHASE);
+  writeMFRC522(CommandReg, PCD_RESETPHASE);
 }
 
 void RFID::writeMFRC522(unsigned char addr, unsigned char val)
 {
-	digitalWrite(_chipSelectPin, LOW);
+  digitalWrite(_chipSelectPin, LOW);
 
-	//0XXXXXX0 formato de dirección
-	SPI.transfer((addr<<1)&0x7E);	
-	SPI.transfer(val);
-	
-	digitalWrite(_chipSelectPin, HIGH);
+  //0XXXXXX0 formato de dirección
+  SPI.transfer((addr<<1)&0x7E);	
+  SPI.transfer(val);
+
+  digitalWrite(_chipSelectPin, HIGH);
 }
 
 void RFID::antennaOn(void)
 {
-	unsigned char temp;
+  unsigned char temp;
 
-	temp = readMFRC522(TxControlReg);
-	if (!(temp & 0x03))
-	{
-		setBitMask(TxControlReg, 0x03);
-	}
+  temp = readMFRC522(TxControlReg);
+  if (!(temp & 0x03))
+  {
+    setBitMask(TxControlReg, 0x03);
+  }
 }
 
 /*
@@ -127,166 +127,166 @@ void RFID::antennaOn(void)
  */
 unsigned char RFID::readMFRC522(unsigned char addr)
 {
-	unsigned char val;
-	digitalWrite(_chipSelectPin, LOW);
-	SPI.transfer(((addr<<1)&0x7E) | 0x80);	
-	val =SPI.transfer(0x00);
-	digitalWrite(_chipSelectPin, HIGH);
-	return val;	
+  unsigned char val;
+  digitalWrite(_chipSelectPin, LOW);
+  SPI.transfer(((addr<<1)&0x7E) | 0x80);	
+  val =SPI.transfer(0x00);
+  digitalWrite(_chipSelectPin, HIGH);
+  return val;	
 }
 
 void RFID::setBitMask(unsigned char reg, unsigned char mask)  
 { 
-    unsigned char tmp;
-    tmp = readMFRC522(reg);
-    writeMFRC522(reg, tmp | mask);  // set bit mask
+  unsigned char tmp;
+  tmp = readMFRC522(reg);
+  writeMFRC522(reg, tmp | mask);  // set bit mask
 }
 
 void RFID::clearBitMask(unsigned char reg, unsigned char mask)  
 {
-    unsigned char tmp;
-    tmp = readMFRC522(reg);
-    writeMFRC522(reg, tmp & (~mask));  // clear bit mask
+  unsigned char tmp;
+  tmp = readMFRC522(reg);
+  writeMFRC522(reg, tmp & (~mask));  // clear bit mask
 } 
 
 void RFID::calculateCRC(unsigned char *pIndata, unsigned char len, unsigned char *pOutData)
 {
-    unsigned char i, n;
+  unsigned char i, n;
 
-    clearBitMask(DivIrqReg, 0x04);			//CRCIrq = 0
-    setBitMask(FIFOLevelReg, 0x80);			//Claro puntero FIFO
-    //Write_MFRC522(CommandReg, PCD_IDLE);
+  clearBitMask(DivIrqReg, 0x04);			//CRCIrq = 0
+  setBitMask(FIFOLevelReg, 0x80);			//Claro puntero FIFO
+  //Write_MFRC522(CommandReg, PCD_IDLE);
 
-	//Escribir datos en el FIFO	
-    for (i=0; i<len; i++)
-    {   
-		writeMFRC522(FIFODataReg, *(pIndata+i));   
-	}
-    writeMFRC522(CommandReg, PCD_CALCCRC);
+  //Escribir datos en el FIFO	
+  for (i=0; i<len; i++)
+  {   
+    writeMFRC522(FIFODataReg, *(pIndata+i));   
+  }
+  writeMFRC522(CommandReg, PCD_CALCCRC);
 
-	// Esperar a la finalización de cálculo del CRC
-    i = 0xFF;
-    do 
-    {
-        n = readMFRC522(DivIrqReg);
-        i--;
-    }
-    while ((i!=0) && !(n&0x04));			//CRCIrq = 1
+  // Esperar a la finalización de cálculo del CRC
+  i = 0xFF;
+  do 
+  {
+    n = readMFRC522(DivIrqReg);
+    i--;
+  }
+  while ((i!=0) && !(n&0x04));			//CRCIrq = 1
 
-	//Lea el cálculo de CRC
-    pOutData[0] = readMFRC522(CRCResultRegL);
-    pOutData[1] = readMFRC522(CRCResultRegM);
+  //Lea el cálculo de CRC
+  pOutData[0] = readMFRC522(CRCResultRegL);
+  pOutData[1] = readMFRC522(CRCResultRegM);
 }
 
 unsigned char RFID::MFRC522ToCard(unsigned char command, unsigned char *sendData, unsigned char sendLen, unsigned char *backData, unsigned int *backLen)
 {
-    unsigned char status = MI_ERR;
-    unsigned char irqEn = 0x00;
-    unsigned char waitIRq = 0x00;
-	unsigned char lastBits;
-    unsigned char n;
-    unsigned int i;
+  unsigned char status = MI_ERR;
+  unsigned char irqEn = 0x00;
+  unsigned char waitIRq = 0x00;
+  unsigned char lastBits;
+  unsigned char n;
+  unsigned int i;
 
-    switch (command)
+  switch (command)
+  {
+    case PCD_AUTHENT:		// Tarjetas de certificación cerca
+      {
+        irqEn = 0x12;
+        waitIRq = 0x10;
+        break;
+      }
+    case PCD_TRANSCEIVE:	//La transmisión de datos FIFO
+      {
+        irqEn = 0x77;
+        waitIRq = 0x30;
+        break;
+      }
+    default:
+      break;
+  }
+
+  writeMFRC522(CommIEnReg, irqEn|0x80);	//De solicitud de interrupción
+  clearBitMask(CommIrqReg, 0x80);			// Borrar todos los bits de petición de interrupción
+  setBitMask(FIFOLevelReg, 0x80);			//FlushBuffer=1, FIFO de inicialización
+
+  writeMFRC522(CommandReg, PCD_IDLE);	//NO action;Y cancelar el comando
+
+  //Escribir datos en el FIFO
+  for (i=0; i<sendLen; i++)
+  {   
+    writeMFRC522(FIFODataReg, sendData[i]);    
+  }
+
+  //???? ejecutar el comando
+  writeMFRC522(CommandReg, command);
+  if (command == PCD_TRANSCEIVE)
+  {    
+    setBitMask(BitFramingReg, 0x80);		//StartSend=1,transmission of data starts  
+  }   
+
+  // A la espera de recibir datos para completar
+  i = 2000;	//i????????,??M1???????25ms	??? i De acuerdo con el ajuste de frecuencia de reloj, el tiempo máximo de espera operación M1 25ms tarjeta??
+  do 
+  {
+    //CommIrqReg[7..0]
+    //Set1 TxIRq RxIRq IdleIRq HiAlerIRq LoAlertIRq ErrIRq TimerIRq
+    n = readMFRC522(CommIrqReg);
+    i--;
+  }
+  while ((i!=0) && !(n&0x01) && !(n&waitIRq));
+
+  clearBitMask(BitFramingReg, 0x80);			//StartSend=0
+
+  if (i != 0)
+  {    
+    if(!(readMFRC522(ErrorReg) & 0x1B))	//BufferOvfl Collerr CRCErr ProtecolErr
     {
-        case PCD_AUTHENT:		// Tarjetas de certificación cerca
-		{
-			irqEn = 0x12;
-			waitIRq = 0x10;
-			break;
-		}
-		case PCD_TRANSCEIVE:	//La transmisión de datos FIFO
-		{
-			irqEn = 0x77;
-			waitIRq = 0x30;
-			break;
-		}
-		default:
-			break;
-    }
-   
-    writeMFRC522(CommIEnReg, irqEn|0x80);	//De solicitud de interrupción
-    clearBitMask(CommIrqReg, 0x80);			// Borrar todos los bits de petición de interrupción
-    setBitMask(FIFOLevelReg, 0x80);			//FlushBuffer=1, FIFO de inicialización
-    
-	writeMFRC522(CommandReg, PCD_IDLE);	//NO action;Y cancelar el comando
+      status = MI_OK;
+      if (n & irqEn & 0x01)
+      {   
+        status = MI_NOTAGERR;			//??   
+      }
 
-	//Escribir datos en el FIFO
-    for (i=0; i<sendLen; i++)
-    {   
-		writeMFRC522(FIFODataReg, sendData[i]);    
-	}
-
-	//???? ejecutar el comando
-	writeMFRC522(CommandReg, command);
-    if (command == PCD_TRANSCEIVE)
-    {    
-		setBitMask(BitFramingReg, 0x80);		//StartSend=1,transmission of data starts  
-	}   
-    
-	// A la espera de recibir datos para completar
-	i = 2000;	//i????????,??M1???????25ms	??? i De acuerdo con el ajuste de frecuencia de reloj, el tiempo máximo de espera operación M1 25ms tarjeta??
-    do 
-    {
-		//CommIrqReg[7..0]
-		//Set1 TxIRq RxIRq IdleIRq HiAlerIRq LoAlertIRq ErrIRq TimerIRq
-        n = readMFRC522(CommIrqReg);
-        i--;
-    }
-    while ((i!=0) && !(n&0x01) && !(n&waitIRq));
-
-    clearBitMask(BitFramingReg, 0x80);			//StartSend=0
-	
-    if (i != 0)
-    {    
-        if(!(readMFRC522(ErrorReg) & 0x1B))	//BufferOvfl Collerr CRCErr ProtecolErr
-        {
-            status = MI_OK;
-            if (n & irqEn & 0x01)
-            {   
-				status = MI_NOTAGERR;			//??   
-			}
-
-            if (command == PCD_TRANSCEIVE)
-            {
-               	n = readMFRC522(FIFOLevelReg);
-              	lastBits = readMFRC522(ControlReg) & 0x07;
-                if (lastBits)
-                {   
-					*backLen = (n-1)*8 + lastBits;   
-				}
-                else
-                {   
-					*backLen = n*8;   
-				}
-
-                if (n == 0)
-                {   
-					n = 1;    
-				}
-                if (n > MAX_LEN)
-                {   
-					n = MAX_LEN;   
-				}
-				
-				//??FIFO??????? Lea los datos recibidos en el FIFO
-                for (i=0; i<n; i++)
-                {   
-					backData[i] = readMFRC522(FIFODataReg);    
-				}
-            }
+      if (command == PCD_TRANSCEIVE)
+      {
+        n = readMFRC522(FIFOLevelReg);
+        lastBits = readMFRC522(ControlReg) & 0x07;
+        if (lastBits)
+        {   
+          *backLen = (n-1)*8 + lastBits;   
         }
         else
         {   
-			status = MI_ERR;  
-		}
-        
-    }
-	
-    //SetBitMask(ControlReg,0x80);           //timer stops
-    //Write_MFRC522(CommandReg, PCD_IDLE); 
+          *backLen = n*8;   
+        }
 
-    return status;
+        if (n == 0)
+        {   
+          n = 1;    
+        }
+        if (n > MAX_LEN)
+        {   
+          n = MAX_LEN;   
+        }
+
+        //??FIFO??????? Lea los datos recibidos en el FIFO
+        for (i=0; i<n; i++)
+        {   
+          backData[i] = readMFRC522(FIFODataReg);    
+        }
+      }
+    }
+    else
+    {   
+      status = MI_ERR;  
+    }
+
+  }
+
+  //SetBitMask(ControlReg,0x80);           //timer stops
+  //Write_MFRC522(CommandReg, PCD_IDLE); 
+
+  return status;
 }
 
 
@@ -304,20 +304,20 @@ unsigned char RFID::MFRC522ToCard(unsigned char command, unsigned char *sendData
  */
 unsigned char  RFID::MFRC522Request(unsigned char reqMode, unsigned char *TagType)
 {
-	unsigned char status;  
-	unsigned int backBits;			//   Recibió bits de datos
+  unsigned char status;  
+  unsigned int backBits;			//   Recibió bits de datos
 
-	writeMFRC522(BitFramingReg, 0x07);		//TxLastBists = BitFramingReg[2..0]	???
-	
-	TagType[0] = reqMode;
-	status = MFRC522ToCard(PCD_TRANSCEIVE, TagType, 1, TagType, &backBits);
+  writeMFRC522(BitFramingReg, 0x07);		//TxLastBists = BitFramingReg[2..0]	???
 
-	if ((status != MI_OK) || (backBits != 0x10))
-	{    
-		status = MI_ERR;
-	}
-   
-	return status;
+  TagType[0] = reqMode;
+  status = MFRC522ToCard(PCD_TRANSCEIVE, TagType, 1, TagType, &backBits);
+
+  if ((status != MI_OK) || (backBits != 0x10))
+  {    
+    status = MI_ERR;
+  }
+
+  return status;
 }
 
 /**
@@ -328,75 +328,75 @@ unsigned char  RFID::MFRC522Request(unsigned char reqMode, unsigned char *TagTyp
  */
 unsigned char RFID::anticoll(unsigned char *serNum)
 {
-    unsigned char status;
-    unsigned char i;
-	unsigned char serNumCheck=0;
-    unsigned int unLen;
-    
+  unsigned char status;
+  unsigned char i;
+  unsigned char serNumCheck=0;
+  unsigned int unLen;
 
-    //ClearBitMask(Status2Reg, 0x08);		//TempSensclear
-    //ClearBitMask(CollReg,0x80);			//ValuesAfterColl
-	writeMFRC522(BitFramingReg, 0x00);		//TxLastBists = BitFramingReg[2..0]
- 
-    serNum[0] = PICC_ANTICOLL;
-    serNum[1] = 0x20;
-    status = MFRC522ToCard(PCD_TRANSCEIVE, serNum, 2, serNum, &unLen);
 
-    if (status == MI_OK)
-	{
-		//?????? Compruebe el número de serie de la tarjeta
-		for (i=0; i<4; i++)
-		{   
-		 	serNumCheck ^= serNum[i];
-		}
-		if (serNumCheck != serNum[i])
-		{   
-			status = MI_ERR;    
-		}
+  //ClearBitMask(Status2Reg, 0x08);		//TempSensclear
+  //ClearBitMask(CollReg,0x80);			//ValuesAfterColl
+  writeMFRC522(BitFramingReg, 0x00);		//TxLastBists = BitFramingReg[2..0]
+
+  serNum[0] = PICC_ANTICOLL;
+  serNum[1] = 0x20;
+  status = MFRC522ToCard(PCD_TRANSCEIVE, serNum, 2, serNum, &unLen);
+
+  if (status == MI_OK)
+  {
+    //?????? Compruebe el número de serie de la tarjeta
+    for (i=0; i<4; i++)
+    {   
+      serNumCheck ^= serNum[i];
     }
+    if (serNumCheck != serNum[i])
+    {   
+      status = MI_ERR;    
+    }
+  }
 
-    //SetBitMask(CollReg, 0x80);		//ValuesAfterColl=1
+  //SetBitMask(CollReg, 0x80);		//ValuesAfterColl=1
 
-    return status;
+  return status;
 }
 
 /* 
  * MFRC522Auth -> auth
  * Verificar la contraseña de la tarjeta
  * Los parámetros de entrada: AuthMode - Modo de autenticación de contraseña
-                 0x60 = A 0x60 = validación KeyA
-                 0x61 = B 0x61 = validación KeyB
-             BlockAddr--  bloque de direcciones
-             Sectorkey-- sector contraseña
-             serNum--,4? Tarjeta de número de serie, 4 bytes
+ 0x60 = A 0x60 = validación KeyA
+ 0x61 = B 0x61 = validación KeyB
+ BlockAddr--  bloque de direcciones
+ Sectorkey-- sector contraseña
+ serNum--,4? Tarjeta de número de serie, 4 bytes
  * MI_OK Valor de retorno: el retorno exitoso MI_OK
  */
 unsigned char RFID::auth(unsigned char authMode, unsigned char BlockAddr, unsigned char *Sectorkey, unsigned char *serNum)
 {
-    unsigned char status;
-    unsigned int recvBits;
-    unsigned char i;
-	unsigned char buff[12]; 
+  unsigned char status;
+  unsigned int recvBits;
+  unsigned char i;
+  unsigned char buff[12]; 
 
-	//????+???+????+???? Verifique la dirección de comandos de bloques del sector + + contraseña + número de la tarjeta de serie
-    buff[0] = authMode;
-    buff[1] = BlockAddr;
-    for (i=0; i<6; i++)
-    {    
-		buff[i+2] = *(Sectorkey+i);   
-	}
-    for (i=0; i<4; i++)
-    {    
-		buff[i+8] = *(serNum+i);   
-	}
-    status = MFRC522ToCard(PCD_AUTHENT, buff, 12, buff, &recvBits);
+  //????+???+????+???? Verifique la dirección de comandos de bloques del sector + + contraseña + número de la tarjeta de serie
+  buff[0] = authMode;
+  buff[1] = BlockAddr;
+  for (i=0; i<6; i++)
+  {    
+    buff[i+2] = *(Sectorkey+i);   
+  }
+  for (i=0; i<4; i++)
+  {    
+    buff[i+8] = *(serNum+i);   
+  }
+  status = MFRC522ToCard(PCD_AUTHENT, buff, 12, buff, &recvBits);
 
-    if ((status != MI_OK) || (!(readMFRC522(Status2Reg) & 0x08)))
-    {   
-		status = MI_ERR;   
-	}
-    
-    return status;
+  if ((status != MI_OK) || (!(readMFRC522(Status2Reg) & 0x08)))
+  {   
+    status = MI_ERR;   
+  }
+
+  return status;
 }
 
 /*
@@ -407,20 +407,20 @@ unsigned char RFID::auth(unsigned char authMode, unsigned char BlockAddr, unsign
  */
 unsigned char RFID::read(unsigned char blockAddr, unsigned char *recvData)
 {
-    unsigned char status;
-    unsigned int unLen;
+  unsigned char status;
+  unsigned int unLen;
 
-    recvData[0] = PICC_READ;
-    recvData[1] = blockAddr;
-    calculateCRC(recvData,2, &recvData[2]);
-    status = MFRC522ToCard(PCD_TRANSCEIVE, recvData, 4, recvData, &unLen);
+  recvData[0] = PICC_READ;
+  recvData[1] = blockAddr;
+  calculateCRC(recvData,2, &recvData[2]);
+  status = MFRC522ToCard(PCD_TRANSCEIVE, recvData, 4, recvData, &unLen);
 
-    if ((status != MI_OK) || (unLen != 0x90))
-    {
-        status = MI_ERR;
-    }
-    
-    return status;
+  if ((status != MI_OK) || (unLen != 0x90))
+  {
+    status = MI_ERR;
+  }
+
+  return status;
 }
 
 /*
@@ -431,37 +431,37 @@ unsigned char RFID::read(unsigned char blockAddr, unsigned char *recvData)
  */
 unsigned char RFID::write(unsigned char blockAddr, unsigned char *writeData)
 {
-    unsigned char status;
-    unsigned int recvBits;
-    unsigned char i;
-	unsigned char buff[18]; 
-    
-    buff[0] = PICC_WRITE;
-    buff[1] = blockAddr;
-    calculateCRC(buff, 2, &buff[2]);
-    status = MFRC522ToCard(PCD_TRANSCEIVE, buff, 4, buff, &recvBits);
+  unsigned char status;
+  unsigned int recvBits;
+  unsigned char i;
+  unsigned char buff[18]; 
+
+  buff[0] = PICC_WRITE;
+  buff[1] = blockAddr;
+  calculateCRC(buff, 2, &buff[2]);
+  status = MFRC522ToCard(PCD_TRANSCEIVE, buff, 4, buff, &recvBits);
+
+  if ((status != MI_OK) || (recvBits != 4) || ((buff[0] & 0x0F) != 0x0A))
+  {   
+    status = MI_ERR;   
+  }
+
+  if (status == MI_OK)
+  {
+    for (i=0; i<16; i++)		//?FIFO?16Byte?? Datos a la FIFO 16Byte escribir
+    {    
+      buff[i] = *(writeData+i);   
+    }
+    calculateCRC(buff, 16, &buff[16]);
+    status = MFRC522ToCard(PCD_TRANSCEIVE, buff, 18, buff, &recvBits);
 
     if ((status != MI_OK) || (recvBits != 4) || ((buff[0] & 0x0F) != 0x0A))
     {   
-		status = MI_ERR;   
-	}
-        
-    if (status == MI_OK)
-    {
-        for (i=0; i<16; i++)		//?FIFO?16Byte?? Datos a la FIFO 16Byte escribir
-        {    
-        	buff[i] = *(writeData+i);   
-        }
-        calculateCRC(buff, 16, &buff[16]);
-        status = MFRC522ToCard(PCD_TRANSCEIVE, buff, 18, buff, &recvBits);
-        
-		if ((status != MI_OK) || (recvBits != 4) || ((buff[0] & 0x0F) != 0x0A))
-        {   
-			status = MI_ERR;   
-		}
+      status = MI_ERR;   
     }
-    
-    return status;
+  }
+
+  return status;
 }
 
 
@@ -473,13 +473,13 @@ unsigned char RFID::write(unsigned char blockAddr, unsigned char *writeData)
  */
 void RFID::halt()
 {
-	unsigned char status;
-    unsigned int unLen;
-    unsigned char buff[4]; 
+  unsigned char status;
+  unsigned int unLen;
+  unsigned char buff[4]; 
 
-    buff[0] = PICC_HALT;
-    buff[1] = 0;
-    calculateCRC(buff, 2, &buff[2]);
- 
-    status = MFRC522ToCard(PCD_TRANSCEIVE, buff, 4, buff,&unLen);
+  buff[0] = PICC_HALT;
+  buff[1] = 0;
+  calculateCRC(buff, 2, &buff[2]);
+
+  status = MFRC522ToCard(PCD_TRANSCEIVE, buff, 4, buff,&unLen);
 }
